901251ae091b1352296a55f775f96920da6e4547
Nataly Ayyad <nayyad@apple.com>
2022-03-10 16:14:11 +0200

    adding livereload

--- out/index.js
+++ out/index.js
@@ -5,9 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
+app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.send('Well done!');
+});
+app.post('/', (req, res) => {
+    res.send({
+        data: req.body
+    });
 });
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 // resource: https://www.youtube.com/watch?v=QqHaDrsj8VA&list=PLsCs8YwnvP88lUF2Z5qRP28pXBh3ef9Rz
 
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
     res.send('Well done!');
+})
+
+app.post('/', (req: Request, res: Response) => {
+    res.send({
+        data: req.body
+    })
 })
 
 app.listen(3000, () => {
